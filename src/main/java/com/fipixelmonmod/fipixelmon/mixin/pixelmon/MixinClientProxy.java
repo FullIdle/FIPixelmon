6fdb5c662fbc7ceeed7411857de7dfb99861bf56
FullIdle <2675423979@qq.com>
2024-03-27 10:38:51 +0800

    整理

--- src/main/java/com/fipixelmonmod/fipixelmon/mixin/pixelmon/MixinClientProxy.java
+++
@@ -1,8 +0,0 @@
-package com.fipixelmonmod.fipixelmon.mixin.pixelmon;
-
-import com.pixelmonmod.pixelmon.client.ClientProxy;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ClientProxy.class)
-public class MixinClientProxy {
-}

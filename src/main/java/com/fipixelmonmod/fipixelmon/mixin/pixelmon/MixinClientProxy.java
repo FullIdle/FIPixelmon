0b9c925b2f0f9430cf34d6f50eba53f5bf4dbfe8
FullIdle <2675423979@qq.com>
2025-01-11 19:36:10 +0800

    修复非zip资源添加非原版精灵配置读取不到问题 修复自定义添加的宝可梦的照片显示不来的问题.

---
+++ src/main/java/com/fipixelmonmod/fipixelmon/mixin/pixelmon/MixinClientProxy.java
@@ -0,0 +1,75 @@
+package com.fipixelmonmod.fipixelmon.mixin.pixelmon;
+
+import com.fipixelmonmod.fipixelmon.FIPixelmon;
+import com.fipixelmonmod.fipixelmon.helper.FileHelper;
+import com.pixelmonmod.pixelmon.client.ClientProxy;
+import com.pixelmonmod.pixelmon.util.helpers.RCFileHelper;
+import lombok.SneakyThrows;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+@Mixin(
+        value = ClientProxy.class,
+        remap = false
+)
+public class MixinClientProxy {
+    @SneakyThrows
+    @Inject(
+            method = "loadSpritesToAtlas",
+            at = @At("HEAD"),
+            remap = false
+    )
+    private void loadSpritesToAtlas(TextureStitchEvent.Pre event, CallbackInfo ci){
+        {//FIPDataFile
+            File file = new File(FIPixelmon.texturesFolder, "sprites");
+            if (file.exists() && file.listFiles() != null) {
+                Path path = file.toPath();
+                List<Path> paths = RCFileHelper.listFilesRecursively(path, entry -> entry.getFileName().toString().endsWith(".png"), true);
+                String fileName;
+                StringBuilder folderName;
+                for (Path pngPath : paths) {
+                    fileName = pngPath.getFileName().toString();
+                    folderName = new StringBuilder(pngPath.getParent().getFileName().toString());
+                    if (folderName.length() > 0) folderName.append("/");
+                    event.getMap().registerSprite(new ResourceLocation("pixelmon", "sprites/" + folderName + fileName.substring(0, fileName.length() - 4)));
+                }
+            }
+        }
+        {//zip
+            ZipFile zipFile;
+            String fileName;
+            StringBuilder folderName;
+            ZipEntry zipEntry;
+            String path;
+            int index;
+            Enumeration<? extends ZipEntry> entries;
+            for (File file : FileHelper.loadedZipFile) {
+                zipFile = new ZipFile(file);
+                entries = zipFile.entries();
+                while (entries.hasMoreElements()) {
+                    zipEntry = entries.nextElement();
+                    path = zipEntry.getName();
+                    if (path.startsWith("textures/sprites/") && path.endsWith(".png")) {
+                        index = path.lastIndexOf('/');
+                        folderName = new StringBuilder(path.substring(path.substring(0, index).lastIndexOf('/') + 1, index));
+                        if (folderName.length() > 0) folderName.append("/");
+                        fileName = path.substring(index + 1, path.length() - 4);
+                        event.getMap().registerSprite(new ResourceLocation("pixelmon", "sprites/"+ folderName + fileName));
+                    }
+                }
+                zipFile.close();
+            }
+        }
+    }
+}

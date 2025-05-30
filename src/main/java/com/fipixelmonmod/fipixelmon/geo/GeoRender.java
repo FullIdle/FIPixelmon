221150b94b46488c5090e4ce00e80d66c71d9a29
FullIdle <2675423979@qq.com>
2025-05-30 21:35:26 +0800

    geo模型渲染修复形态渲染

--- src/main/java/com/fipixelmonmod/fipixelmon/geo/GeoRender.java
+++ src/main/java/com/fipixelmonmod/fipixelmon/geo/GeoRender.java
@@ -1,8 +1,10 @@
 package com.fipixelmonmod.fipixelmon.geo;
 
 import com.fipixelmonmod.fipixelmon.data.PokemonConfig;
+import com.fipixelmonmod.fipixelmon.enums.EnumForm;
 import com.pixelmonmod.pixelmon.client.render.RenderPixelmon;
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
+import com.pixelmonmod.pixelmon.enums.forms.EnumNoForm;
 import lombok.val;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.EntityLivingBase;
@@ -20,14 +22,24 @@ public class GeoRender extends GeoEntityRenderer {
     public void doRender(EntityLivingBase entity, double x, double y, double z, float entityYaw, float partialTicks) {
         val pixelmon = (EntityPixelmon) entity;
         val config = PokemonConfig.extraPokemonConfig.get(pixelmon.getSpecies());
-        if (config == null || config.getGeoModel() == null) {
+
+        if (config == null || !hasGeoModel(pixelmon, config)) {
             oldRender.doRender(pixelmon, x, y, z, entityYaw, partialTicks);
             return;
         }
+
         super.doRender(pixelmon, x, y, z, entityYaw, partialTicks);
 
 
         //TODO geo不会渲染的东西调用原版 (可能不全)
         oldRender.renderName(pixelmon, x, y, z);
+    }
+
+
+    public static boolean hasGeoModel(EntityPixelmon pixelmon, PokemonConfig config) {
+        if (pixelmon.getFormEnum() == EnumNoForm.NoForm) return config.getGeoModel() != null;
+        if (pixelmon.getFormEnum() instanceof EnumForm)
+            return ((EnumForm) pixelmon.getFormEnum()).getData().getGeoModel() != null;
+        return false;
     }
 }

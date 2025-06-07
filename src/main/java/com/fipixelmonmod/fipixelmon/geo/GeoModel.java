5f7f98f2f669995996109f433e4827742f7c0118
FullIdle <2675423979@qq.com>
2025-06-08 01:25:38 +0800

    证实没有头部，无法实现

--- src/main/java/com/fipixelmonmod/fipixelmon/geo/GeoModel.java
+++ src/main/java/com/fipixelmonmod/fipixelmon/geo/GeoModel.java
@@ -5,7 +5,14 @@ import com.fipixelmonmod.fipixelmon.enums.EnumForm;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
 
 public class GeoModel extends AnimatedGeoModel {
     @Override
@@ -49,5 +56,48 @@ public class GeoModel extends AnimatedGeoModel {
         if (entity.getFormEnum() instanceof EnumForm)
             return ((EnumForm) entity.getFormEnum()).getData().getGeoAnimation();
         return config.getGeoAnimation();
+    }
+
+
+    /**
+     * TODO 似乎pixelmon本社那就没有头部，可能需要手动实现头部的存在...
+     * 灵活头部的实现
+     */
+    @Override
+    public void setLivingAnimations(IAnimatable entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        extendLivingAnimations((EntityPixelmon) entity);
+    }
+
+    private void extendLivingAnimations(EntityPixelmon entity) {
+        nonNullExec(this.getAnimationProcessor().getBone("head"), bone -> {
+            setHeadRotation(entity, bone);
+            return bone;
+        });
+    }
+
+    private void setHeadRotation(EntityPixelmon entity, IBone head) {
+        head.setRotationX(formatPitch(entity.rotationPitch));
+        head.setRotationY(formatYaw(entity.rotationYaw - entity.prevRotationYawHead));
+    }
+
+    private float formatPitch(float pitch) {
+        return (float) Math.toRadians(MathHelper.clamp(-pitch, -90, 90));
+    }
+
+    private float formatYaw(float yaw) {
+        return (float) Math.toRadians(yaw);
+    }
+
+    /**
+     * 非空调用
+     *
+     * @param v        被处理对象
+     * @param function 非空时调用的函数
+     * @return 函数处理后的返回值
+     */
+    private <T, E> E nonNullExec(T v, Function<T, E> function) {
+        if (v == null) return null;
+        return function.apply(v);
     }
 }

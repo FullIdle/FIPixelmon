d231ae3f7964342817cf2b101db73683f42d7c57
FullIdle <2675423979@qq.com>
2026-01-03 03:19:11 +0800

    修复，某些物种比如TapuKoko这种实际中文命中间会有一个点或者特殊中文名的物种的baseStats获取不到对应物种的问题 将物种适配器从重铸中的移除(以前的垃圾) 将多余的mixin类去除

--- src/main/java/com/fipixelmonmod/fipixelmon/mixin/pixelmon/MixinBaseStatsLoader.java
+++ src/main/java/com/fipixelmonmod/fipixelmon/mixin/pixelmon/MixinBaseStatsLoader.java
@@ -2,26 +2,13 @@ package com.fipixelmonmod.fipixelmon.mixin.pixelmon;
 
 
 import com.fipixelmonmod.fipixelmon.FIPixelmon;
-import com.fipixelmonmod.fipixelmon.adapter.EnumSpeciesAdapter;
 import com.fipixelmonmod.fipixelmon.data.PokemonConfig;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
-import com.pixelmonmod.pixelmon.battles.attacks.Attack;
-import com.pixelmonmod.pixelmon.battles.attacks.AttackBase;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.AttackBaseAdapter;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.AttackTypeAdapter;
 import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BaseStats;
 import com.pixelmonmod.pixelmon.entities.pixelmon.stats.BaseStatsLoader;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.evolution.EvoConditionTypeAdapter;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.evolution.Evolution;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.evolution.EvolutionTypeAdapter;
-import com.pixelmonmod.pixelmon.entities.pixelmon.stats.evolution.conditions.EvoCondition;
 import com.pixelmonmod.pixelmon.enums.EnumSpecies;
-import com.pixelmonmod.pixelmon.enums.technicalmoves.ITechnicalMove;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -36,24 +23,11 @@ import java.util.zip.ZipFile;
 
 @Mixin(value = BaseStatsLoader.class, remap = false)
 public class MixinBaseStatsLoader {
-    @Mutable
-    @Shadow
-    @Final
-    public static transient Gson GSON = (new GsonBuilder())
-            .setPrettyPrinting()
-            //增加物种适配器
-            .registerTypeAdapter(EnumSpecies.class, EnumSpeciesAdapter.INSTANCE)
-            .registerTypeAdapter(Evolution.class, new EvolutionTypeAdapter())
-            .registerTypeAdapter(EvoCondition.class, new EvoConditionTypeAdapter())
-            .registerTypeAdapter(Attack.class, new AttackTypeAdapter())
-            .registerTypeAdapter(AttackBase.class, new AttackBaseAdapter())
-            .registerTypeAdapter(PokemonSpec.class, PokemonSpec.SPEC_ADAPTER)
-            .registerTypeHierarchyAdapter(ITechnicalMove.class, new ITechnicalMove.Adapter())
-            .create();
-
     @Shadow
     private static void prepare(EnumSpecies species, BaseStats bs) {
     }
+
+    @Shadow @Final public static transient Gson GSON;
 
     @Inject(
             method = "getBaseStatsFromAssets",
